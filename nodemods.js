0743930a2b9ed1590cd72d036db888b9e78b8864
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-09-17 17:06:27 -0500

    Finish tic tock functionality.

--- nodemods.js
+++ nodemods.js
@@ -2,10 +2,16 @@ var os = require('os');
 
 var dns = require('dns');
 
-var timers = require('timers');
-
 var w3 = dns.lookup('w3schools.com', function (err, addresses, family) {
   console.log(addresses);
 });
 
 console.log(os.freemem());
+
+var tic = setInterval(() => {
+  console.log("tic")
+}, 1000);
+
+var tock = setInterval(() => {
+  console.log("tock")
+}, 1000);
